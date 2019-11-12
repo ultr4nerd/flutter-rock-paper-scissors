332c95f9a10a3879ac0990ab63c066c2177f0e63
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-11-12 03:17:41 -0600

    Hello, Stateful Widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
 
   HomePage({Key key, this.title}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String greeting = '¡Hola mundo desde Flutter!';
+
+  void changeTitle() {
+    this.setState(() {
+      if (greeting == '¡Hola mundo desde Flutter!') {
+        this.greeting = '¡Hola mundo desde Stateful Widget!';
+      } else {
+        this.greeting = '¡Hola mundo desde Flutter!';
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.title),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Text('¡Hola mundo desde Flutter!'),
+        child: Text(this.greeting),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeTitle,
+        tooltip: 'Cambiar',
+        child: Icon(Icons.swap_horiz),
       ),
     );
   }
 }
-  
